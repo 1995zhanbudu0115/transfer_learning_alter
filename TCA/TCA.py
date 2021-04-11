54a2b2dd0d3b7743f6127fff5f96f2e5ca829fc9
1995zhanbuduens0115 <632659849@qq.com>
2021-04-11 18:58:52 +0800

    简化TCA

--- TCA/TCA.py
+++ TCA/TCA.py
@@ -10,6 +10,7 @@ import sklearn.metrics
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
+
 def kernel(ker, X1, X2, gamma):
     K = None
     if not ker or ker == 'primal':
@@ -28,7 +29,7 @@ def kernel(ker, X1, X2, gamma):
 
 
 class TCA:
-    def __init__(self, kernel_type='primal', dim=30, lamb=1, gamma=1):
+    def __init__(self, kernel_type='primal', dim=5, lamb=1, gamma=1):
         '''
         Init func
         :param kernel_type: kernel, values: 'primal' | 'linear' | 'rbf'
@@ -40,52 +41,43 @@ class TCA:
         self.dim = dim
         self.lamb = lamb
         self.gamma = gamma
+        self.A = None
+        self.X = None
 
-    def fit(self, Xs, Xt):
+    def fit_transform(self, Xs, Xt):
         '''
         Transform Xs and Xt
         :param Xs: ns * n_feature, source feature
         :param Xt: nt * n_feature, target feature
         :return: Xs_new and Xt_new after TCA
         '''
-        X = np.hstack((Xs.T, Xt.T))
-        X /= np.linalg.norm(X, axis=0)
-        m, n = X.shape
+        self.X = np.hstack((Xs.T, Xt.T))
+        self.X /= np.linalg.norm(self.X, axis=0)
+        m, n = self.X.shape
         ns, nt = len(Xs), len(Xt)
+        # 求L矩阵
+        # (1) 计算论文中的1/n1 与 1/n2，n1表示的是源域的长度，n2表示的是目标域的长度
         e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        # (2) 计算1/n1^2和1/n2^2
         M = e * e.T
         M = M / np.linalg.norm(M, 'fro')
+        # 求中心矩阵H
         H = np.eye(n) - 1 / n * np.ones((n, n))
-        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+        # 求核矩阵
+        K = kernel(self.kernel_type, self.X, None, gamma=self.gamma)
         n_eye = m if self.kernel_type == 'primal' else n
         a, b = np.linalg.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+        # 特征值求解，求权重W
         w, V = scipy.linalg.eig(a, b)
         ind = np.argsort(w)
-        A = V[:, ind[:self.dim]]
-        Z = np.dot(A.T, K)
+        self.A = V[:, ind[:self.dim]]
+        Z = np.dot(self.A.T, K)
         Z /= np.linalg.norm(Z, axis=0)
         
         Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
         return Xs_new, Xt_new
 
-    def fit_predict(self, Xs, Ys, Xt, Yt):
-        '''
-        Transform Xs and Xt, then make predictions on target using 1NN
-        :param Xs: ns * n_feature, source feature
-        :param Ys: ns * 1, source label
-        :param Xt: nt * n_feature, target feature
-        :param Yt: nt * 1, target label
-        :return: Accuracy and predicted_labels on the target domain
-        '''
-        Xs_new, Xt_new = self.fit(Xs, Xt)
-        clf = KNeighborsClassifier(n_neighbors=1)
-        clf.fit(Xs_new, Ys.ravel())
-        y_pred = clf.predict(Xt_new)
-        acc = sklearn.metrics.accuracy_score(Yt, y_pred)
-       
-        return acc, y_pred
-
-    def fit_new(self, Xs, Xt, Xt2):
+    def transform(self, Xt2):
         '''
         Map Xt2 to the latent space created from Xt and Xs
         :param Xs : ns * n_feature, source feature
@@ -93,72 +85,63 @@ class TCA:
         :param Xt2: n_s, n_feature, target feature to be mapped
         :return: Xt2_new, mapped Xt2 with projection created by Xs and Xt
         '''
-        # Computing projection matrix A from Xs an Xt
-        X = np.hstack((Xs.T, Xt.T))
-        X /= np.linalg.norm(X, axis=0)
-        m, n = X.shape
-        ns, nt = len(Xs), len(Xt)
-        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
-        M = e * e.T
-        M = M / np.linalg.norm(M, 'fro')
-        H = np.eye(n) - 1 / n * np.ones((n, n))
-        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
-        n_eye = m if self.kernel_type == 'primal' else n
-        a, b = np.linalg.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
-        w, V = scipy.linalg.eig(a, b)
-        ind = np.argsort(w)
-        A = V[:, ind[:self.dim]]
         
         # Compute kernel with Xt2 as target and X as source
         Xt2 = Xt2.T
-        K = kernel(self.kernel_type, X1 = Xt2, X2 = X, gamma=self.gamma)
+        K = kernel(self.kernel_type, X1 = Xt2, X2 = self.X, gamma=self.gamma)
         
         # New target features
-        Xt2_new = K @ A
+        Xt2_new = K @ self.A
         
         return Xt2_new
-    
-    def fit_predict_new(self, Xt, Xs, Ys, Xt2, Yt2):
-        '''
-        Transfrom Xt and Xs, get Xs_new
-        Transform Xt2 with projection matrix created by Xs and Xt, get Xt2_new
-        Make predictions on Xt2_new using classifier trained on Xs_new
-        :param Xt: ns * n_feature, target feature
-        :param Xs: ns * n_feature, source feature
-        :param Ys: ns * 1, source label
-        :param Xt2: nt * n_feature, new target feature
-        :param Yt2: nt * 1, new target label
-        :return: Accuracy and predicted_labels on the target domain
-        '''
-        Xs_new, _ = self.fit(Xs, Xt)
-        Xt2_new   = self.fit_new(Xs, Xt, Xt2)
-        clf = KNeighborsClassifier(n_neighbors=1)
-        clf.fit(Xs_new, Ys.ravel())
-        y_pred = clf.predict(Xt2_new)
-        acc = sklearn.metrics.accuracy_score(Yt2, y_pred)
-        
-        return acc, y_pred
-    
-    
 
+    
+    
 if __name__ == '__main__':
-    domains = ['caltech.mat', 'amazon.mat', 'webcam.mat', 'dslr.mat']
-    for i in [1]:
-        for j in [2]:
-            if i != j:
-                src, tar = 'data/' + domains[i], 'data/' + domains[j]
-                src_domain, tar_domain = scipy.io.loadmat(src), scipy.io.loadmat(tar)
-                Xs, Ys, Xt, Yt = src_domain['feas'], src_domain['labels'], tar_domain['feas'], tar_domain['labels']
-                
-                # Split target data
-                Xt1, Xt2, Yt1, Yt2  = train_test_split(Xt, Yt, train_size=50, stratify=Yt, random_state=42)
-                
-                # Create latent space and evaluate using Xs and Xt1
-                tca = TCA(kernel_type='linear', dim=30, lamb=1, gamma=1)
-                acc1, ypre1 = tca.fit_predict(Xs, Ys, Xt1, Yt1)
-                
-                # Project and evaluate Xt2 existing projection matrix and classifier
-                acc2, ypre2 = tca.fit_predict_new(Xt1, Xs, Ys, Xt2, Yt2)
-                
-    print(f'Accuracy of mapped source and target1 data : {acc1:.3f}') #0.800
-    print(f'Accuracy of mapped target2 data            : {acc2:.3f}') #0.706
+    # import pandas as pd
+    from sklearn.datasets import make_classification
+    from lightgbm import LGBMClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.metrics import roc_auc_score
+    import pickle
+
+    Xs,Ys = make_classification(6000,30,weights=[0.1,0.9],random_state=0)
+    Xt,Yt = make_classification(1000,30,weights=[0.1,0.9],random_state=1)
+    
+    pickle.dump(Xs,open('Xs.pkl','wb+'))
+    pickle.dump(Ys,open('Ys.pkl','wb+'))
+    pickle.dump(Xt,open('Xt.pkl','wb+'))
+    pickle.dump(Yt,open('Yt.pkl','wb+'))
+
+    # Xs = pickle.load(open('Xs.pkl','rb'))
+    # Ys = pickle.load(open('Ys.pkl','rb'))
+    # Xt = pickle.load(open('Xt.pkl','rb'))
+    # Yt = pickle.load(open('Yt.pkl','rb'))
+
+    # src_domain, tar_domain = scipy.io.loadmat(src), scipy.io.loadmat(tar)
+    # Xs, Ys, Xt, Yt = src_domain['feas'], src_domain['labels'], tar_domain['feas'], tar_domain['labels']
+    
+    # Split target data
+    Xt1, Xt2, Yt1, Yt2  = train_test_split(Xt, Yt, train_size=0.5, shuffle=True, random_state=42)
+    
+    # # Create latent space and evaluate using Xs and Xt1
+    tca = TCA(kernel_type='linear', dim=8, lamb=1, gamma=1)
+    Xs_new,Xt1_new = tca.fit_transform(Xs, Xt1)
+    # # Project and evaluate Xt2 existing projection matrix and classifier
+    Xt2_new = tca.transform(Xt2)
+
+    clf = RandomForestClassifier()
+    bse_train_x = np.concatenate([Xs,Xt1],axis=0)
+    bse_train_y = np.concatenate([Ys,Yt1],axis=0)
+    clf.fit(bse_train_x,bse_train_y)
+    pred_tar2 = clf.predict_proba(Xt2)[:,1]
+    print(f'baseline_auc: {roc_auc_score(Yt2,pred_tar2)}')
+
+    bse_x_new = np.concatenate([Xs_new,Xt1_new],axis=0)
+    clf.fit(bse_x_new,bse_train_y)
+    pred_tar1 = clf.predict_proba(Xt1_new)[:,1]
+    pred_tar2 = clf.predict_proba(Xt2_new)[:,1]
+    auc_tar1 = roc_auc_score(Yt1,pred_tar1)
+    auc_tar2 = roc_auc_score(Yt2,pred_tar2)
+    print(f'AUC of mapped source and target1 data : {auc_tar1:.3f}') 
+    print(f'AUC of mapped target2 data            : {auc_tar2:.3f}') 
